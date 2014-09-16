114f99a14725d789414535a0db165bcbc876d7dd
Eric Hulser <ehulser@teslamotors.com>
2014-09-16 10:32:28 -0700

    supporting all modules despite the lazy load for them within the PyInstaller system.

--- src/xqt/pyi_hook.py
+++ src/xqt/pyi_hook.py
@@ -19,3 +19,16 @@ import projex.pyi
 basepath = os.path.dirname(__file__)
 hiddenimports, datas = projex.pyi.collect(basepath)
 excludes = []
+
+# include specific modules
+available_wrappers = ['PyQt4', 'PySide']
+available_modules = ['QtCore', 'QtGui', 'QtNetwork', 'QtWebKit', 'QtXml', 'QtDesigner', 'Qsci']
+chosen_wrapper = os.environ.get('XQT_WRAPPER', 'PyQt4')
+
+for wrapper in available_wrappers:
+    for module in available_modules:
+        modname = '{0}.{1}'.format(wrapper, module)
+        add_to = hiddenimports if wrapper == chosen_wrapper else excludes
+        if not modname in add_to:
+            add_to.append(modname)
+
