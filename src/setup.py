885a20be06be64bf68484d442e271d5e4519cbb2
Eric Hulser <eric.hulser@projexsoftware.com>
2014-12-19 12:01:43 -0800

    updating the setup file for proper bundling of information

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,21 @@
 import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
 try:
-    from setuptools import setup
-except ImportError:
-    try:
-        from distutils.core import setup
-    except ImportError:
-        raise ImportError('Could not find setuptools.')
+    with open(os.path.join(here, 'README.md')) as f:
+        README = f.read()
+except IOError:
+    README = ''
+
+try:
+    REQUIREMENTS = xqt.__depends__
+except AttributeError:
+    REQUIREMENTS = []
 
 setup(
-    name = 'projex_xqt',
-    version = '1.1',
+    name = 'xqt',
+    version = '2.0.2',
     author = 'Projex Software',
     author_email = 'team@projexsoftware.com',
     maintainer = 'Projex Software',
@@ -17,16 +23,11 @@ setup(
     description = '''''',
     license = 'LGPL',
     keywords = '',
-    url = '',
+    url = 'http://www.projexsoftware.com',
     include_package_data=True,
-    long_description='''
-The xqt library is a simple wrapper system on top of the various bindings
-that Qt has for Python.  This provides a way to consolidate the differences
-between things like PyQt4, PyQt5, and PySide in a way that will allow Python
-developers to build tools and libraries that are able to work with any of the
-wrapper systems.
-''',
+    packages = find_packages(),
+    install_requires = REQUIREMENTS,
+    tests_require = REQUIREMENTS,
+    long_description= README,
     classifiers=[],
-    packages = [r'xqt'],
-    package_data = {}
 )
